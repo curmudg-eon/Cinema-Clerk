6ff4b09136d32b6491f95dcf1c278b606218aae5
Gabe Secula <gabe.secula@gmail.com>
2020-05-18 21:27:47 -0400

    Small changes

--- Sources/Cinema-Clerk/main.swift
+++ Sources/Cinema-Clerk/main.swift
@@ -10,16 +10,34 @@ bot.on(.messageCreate) { data in //Message Handler
     let msg = data as! Message
     
     if msg.content.hasPrefix(">")  {
+        var f = msg.content.firstIndex(of: "p")
         var content = msg.content.lowercased()
-        if content.hasPrefix(">help") {
-            reply(msg: msg)
+        switch content {
+        case ">help":
+            helpMessage(msg: msg)
+        default:
+            msg.reply(with: "Failure")
         }
+        
+        /*
+        if content.hasPrefix(">help") {
+            helpMessage(msg: msg)
+        }*/
         
     }
 }
 
-func reply(msg: Message) {
-    
+func helpMessage(msg: Message) {
+    msg.reply(with:"""
+```
+Very well, here are your options:
+>help : Get a list of commands this bot can execute. \n
+>openVoting : Opens a new watchlist for users to add movies. \n
+>addMovie [name] (link) : Adds a movie named [name] to current watchlist. Optionally, add (link) to attach a link to the movie. \n
+>dice : closes voting on current watchlist and picks a movie at random. \n
+```
+""")
 }
 
 bot.connect()
+print("connected as \(bot)")
