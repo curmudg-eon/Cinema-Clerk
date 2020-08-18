c68a7bf86d37e9ca31839c9ba74d90b3434b84f9
Gabe Secula <gabe.secula@gmail.com>
2020-08-18 15:17:25 -0400

    Checking out

--- Package.swift
+++ Package.swift
@@ -6,13 +6,15 @@ import PackageDescription
 let package = Package(
     name: "Cinema-Clerk",
     dependencies: [
-        .package(url: "https://github.com/Azoy/Sword", from: "0.9.0"),
-        .package(name: "SQLite", url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.12.0")
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Cinema-Clerk",
-            dependencies: ["Sword", "SQLite.swift.git"]),
+            dependencies: []),
         .testTarget(
             name: "Cinema-ClerkTests",
             dependencies: ["Cinema-Clerk"]),
