dbfff3c3a4c2bf170e293e030392654831a59469
Gabe Secula <gabe.secula@gmail.com>
2020-09-14 14:44:20 -0400

    Adding new revision to test for Azoy.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Cinema-Clerk",
     dependencies: [
-        .package(url: "https://github.com/Azoy/Sword", from: "0.9.2"),
+        .package(url: "https://github.com/Azoy/Sword", .revision("d53918cb208e422e9b32a472a71e9531df0fb741")), //from: "0.9.2" .revision("d53918cb208e422e9b32a472a71e9531df0fb741")
         .package(name: "SQLite.swift", url: "https://github.com/stephencelis/SQLite.swift", from: "0.12.0"),
     ],
     targets: [
