547b8f10f9c2d697f40026f4374a8ca097c53b84
Gabe Secula <gabe.secula@gmail.com>
2020-10-19 16:08:52 -0400

    Removing arbitrary comments.

--- Sources/Cinema-Clerk/Model/CinemaClerk.swift
+++ Sources/Cinema-Clerk/Model/CinemaClerk.swift
@@ -3,19 +3,10 @@ import Foundation
 class CinemaClerk {
     var streamingLink: String = "No streaming link"
     var movieLists: [String:[WatchPick]] = ["votingList": [WatchPick](), "watchedList": [WatchPick]()]
-    //var showList: [String] = []
-    //var videos: [String] = []
     
     init() {
         
     }
-    
-//  This is probably dumb and won't work but I want to think about it.
-//    enum lists: String {
-//        case votingList = "votingList"
-//        case watchList = "watchList"
-//        case watchedList = "watchedList"
-//    }
        
     func getList(name: String) -> [WatchPick] {
         return movieLists[name]!
